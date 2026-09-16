619ca1f3bc42577df2eddf38c3d42e21b673b01a
ArkanKira <arkanbasha140@gmail.com>
2026-09-16 14:13:00 +0300

    feat: add cart

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,7 @@ import adminRoutes from "./routes/admin.routes.js";
 import productRoutes from "./routes/product.routes.js";
 import categoryRoutes from "./routes/category.routes.js";
 import errorHandler from "./middleware/errorHandler.js";
+import cartRoutes from "./routes/cart.routes.js";
 
 const app = express();
 
@@ -51,6 +52,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/categories", categoryRoutes);
+app.use("/api/cart", cartRoutes);
 
 app.use(errorHandler);
 
