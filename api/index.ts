e38edcebb697758673fb8429bd851774aa6a36a0
Ahmed Hassan <ahmedhassan8754321@gmail.com>
2026-09-20 19:46:17 +0300

    Create index.ts

---
+++ api/index.ts
@@ -0,0 +1,19 @@
+import "dotenv/config";
+import app from "../src/app.js";
+import connectDB from "../src/config/db.js";
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+let isConnected = false;
+
+export default async function handler(req: any, res: any) {
+  if (!isConnected) {
+    await connectDB();
+    isConnected = true;
+  }
+  return app(req, res);
+}
